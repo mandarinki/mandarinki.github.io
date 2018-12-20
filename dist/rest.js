5633d199cb9cfc93277784fb4701527338c82a5c
Self <benevolent.boondoggle@gmail.com>
2018-12-20 16:10:22 +0200

    finished animation

---
+++ dist/rest.js
@@ -0,0 +1,5 @@
+function showIt() {
+  document.getElementById("zero").style.display = "none";
+  document.getElementById("mandarin").style.display = "inline-block";
+}
+setTimeout("showIt()", 2000); // after 1 sec
